a6fb4cdaa023e219afd0de80b362d0397843f4e8
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-10-21 10:09:08 +0300

    pitch created

--- simulator/code.js
+++ simulator/code.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.heigth = window.innerHeight;
+const pitchWidth = 540;
+const pitchHeigth = 360;
 
-ctx.fillStyle = 'red';
-ctx.fillRect(50, 50, 300, 200);
+ctx.fillStyle = 'green';
+ctx.fillRect(0, 0, pitchWidth, pitchHeigth);
