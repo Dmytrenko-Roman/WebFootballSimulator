92e4c4496af2c1e918cb90c26ccd5cad05b64927
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-10-22 12:43:25 +0300

    grid added

--- simulator/code.js
+++ simulator/code.js
@@ -2,6 +2,7 @@
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const checkbox = document.getElementById('grid');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -9,11 +10,22 @@ canvas.height = window.innerHeight;
 let speed = 1;
 
 // Pitch params:
-
 const wp = 600;
 const hp = 400;
 const xp = (canvas.width / 2) - (wp / 2);
 const yp = (canvas.height / 2) - (hp / 2);
+
+// First penalty area params:
+const xpa1 = xp;
+const ypa1 = canvas.height / 2 - 110;
+
+// Second penalty area params:
+const xpa2 = xp + wp - 130;
+const ypa2 = canvas.height / 2 - 110;
+
+// Width and heigth of penalty area:
+const wpa = 130;
+const hpa = 220;
 
 // -------------
 
@@ -35,6 +47,23 @@ const players2 = [
   },
 ];
 
+const grid = () => {
+  ctx.beginPath();
+  ctx.fillStyle = 'green';
+  let grid = [];
+  for (let i = 0; i < wp; i += 20) {
+    for (let k = 0; k < hp; k += 20) {
+      ctx.fillRect(xp + i, yp + k, 20, 20);
+      ctx.strokeStyle = 'black';
+      ctx.rect(xp + i, yp + k, 20, 20);
+      ctx.stroke();
+      grid.push([xp + i, yp + k, 20, 20])
+    }
+  }
+  ctx.closePath();
+  //console.log(grid);
+}
+
 const pitch = () => {
   ctx.beginPath();
   ctx.fillStyle = 'green';
@@ -53,15 +82,22 @@ const pitch = () => {
   ctx.arc(canvas.width / 2, canvas.height / 2, 60, 0, Math.PI * 2);
   ctx.moveTo(canvas.width / 2, yp + 1);
   ctx.lineTo(canvas.width / 2, yp + 400);
-  ctx.rect(xp + 1, yp + 1, wp, hp);
+  ctx.rect(xp, yp, wp, hp);
+  ctx.closePath();
+  ctx.strokeStyle = 'white';
+  ctx.stroke();
   // Penalty areas:
-  ctx.rect(xp + 1, canvas.height / 2 - 110, 130, 220);
-  ctx.rect(xp + 1, canvas.height / 2 - 40, 45, 80);
-  ctx.ellipse(xp + 1 + wp - 130, canvas.height / 2, 30, 40, 0, Math.PI / 2, -Math.PI / 2);
-  ctx.rect(xp + 1 + wp - 130, canvas.height / 2 - 110, 130, 220);
-  ctx.rect(xp + 1 + wp - 45, canvas.height / 2 - 40, 45, 80);
-  //ctx.arc(canvas.width / 2, canvas.height / 2, 60, 0, Math.PI * 2);
-
+  ctx.beginPath();
+  ctx.ellipse(xp + 130, canvas.height / 2, 30, 40, 0, -Math.PI / 2, Math.PI / 2);
+  ctx.closePath();
+  ctx.strokeStyle = 'white';
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.ellipse(xp + wp - 130, canvas.height / 2, 30, 40, 0, Math.PI / 2, -Math.PI / 2);
+  ctx.rect(xpa1, ypa1, wpa, hpa);
+  ctx.rect(xp, canvas.height / 2 - 40, 45, 80);
+  ctx.rect(xpa2, ypa2, wpa, hpa);
+  ctx.rect(xp + wp - 45, canvas.height / 2 - 40, 45, 80);
   ctx.closePath();
   ctx.strokeStyle = 'white';
   ctx.stroke();
@@ -124,9 +160,9 @@ const movePlayers2 = () => {
   }
 };
 
-
 const tick = () => {
   pitch();
+  if (checkbox.checked) grid();
   movePlayers1();
   movePlayers2();
   drawPlayers1();
